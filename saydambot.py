aa9124d23926ab334d8cfbf3fbb8224663570b4d
kaannsaydamm <121751191+kaannsaydamm@users.noreply.github.com>
2023-02-06 12:08:09 +0300

    Create saydambot.py

---
+++ saydambot.py
@@ -0,0 +1,22 @@
+import openai
+
+openai.api_key = "kendi_keyini_gir"
+
+def generate_answer(question):
+    response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=question,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+    answer = response["choices"][0]["text"].strip()
+    return answer
+
+
+while True:
+    question = input("You: ")
+    answer = generate_answer(question)
+    print("Bot: ", answer)
+
